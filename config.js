cf9beb257be6e6cb3bd31a3ebcf55b446ca0ac36
Thanhtu17 <96621749+Thanhtu17@users.noreply.github.com>
2021-12-24 22:08:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Babi của anh",
+    introTitle: 'Văn Phạm Minh Thanh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Cùng anh bước tiếp chặng đường của đôi ta nhé:3 <3',
+    btnReply: 'Gửi cho anhiu của em <3',
+    reply: 'Daaaa em đồng ý <33333333',
+    mess: 'Hihi anh biết mà 🥰. Yêu công chúa của anh nhiều 😘😘',
+    messDesc: 'Nắm chắc tay anh đừng buông ra nha:33.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/TuThanhh/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.anhyeuem
 }
